64df712320507973398b1938512250bf76888f75
William Gutierrez <williamg.dev@gmail.com>
2017-08-22 17:32:21 -0500

    Open Scan Integration

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 #QR Example using Zxing
-In this url you will find the app BarCode which this projetc is integrated.
-``
-https://github.com/williamgdev/QRExampleZxing/blob/master/AppIntegration/BarcodeScanner-4.7.6.apk
-``
+
+
+In this url you will find the app [BarCode] which this project is integrated.
+
+
+
+
+
+
+
+
+
+
+
+
+[BarCode]: <https://github.com/williamgdev/QRExampleZxing/blob/master/AppIntegration/BarcodeScanner-4.7.6.apk>
--- app/src/main/java/com/github/williamgdev/example/qrexamplezxing/MainActivity.java
+++ app/src/main/java/com/github/williamgdev/example/qrexamplezxing/MainActivity.java
@@ -3,11 +3,20 @@ package com.github.williamgdev.example.qrexamplezxing;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onScan();
     }
+
+    private void onScan() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+    }
+
 }
