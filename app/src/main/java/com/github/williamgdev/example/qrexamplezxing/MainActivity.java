3fc5c98de9e01af77e0eba249c0d76c24f7ef67c
William Gutierrez <williamg.dev@gmail.com>
2017-08-22 17:33:22 -0500

    Get Content from AntivityonResult

--- app/src/main/java/com/github/williamgdev/example/qrexamplezxing/MainActivity.java
+++ app/src/main/java/com/github/williamgdev/example/qrexamplezxing/MainActivity.java
@@ -1,11 +1,16 @@
 package com.github.williamgdev.example.qrexamplezxing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "QRScan ->";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,20 @@ public class MainActivity extends AppCompatActivity {
     private void onScan() {
         IntentIntegrator integrator = new IntentIntegrator(this);
         integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (intentResult != null){
+            if (intentResult.getContents() != null){
+                Log.d(TAG, "onActivityResult: Result -> " + intentResult.getContents().toString());
+            } else {
+                Log.d(TAG, "onActivityResult: Result NULL");
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
 }
